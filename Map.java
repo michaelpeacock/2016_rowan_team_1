aeb7bee3c3fa483234d448fe9467af9caf41d68d
Paul K Turner <turner.paul.k@gmail.com>
2016-12-16 16:40:45 -0500

    working on the Map.java

---
+++ Map.java
@@ -0,0 +1,31 @@
+public class Map{
+	
+	double north;
+	double south;
+	double east;
+	double west;	
+
+	public Map(){
+		north = 0;
+		south = 0;
+		east = 0;
+		west = 0;
+
+		
+	}
+
+	public Map(Point points[]){
+		
+		
+
+	}
+
+	public Map(double north, double south, double east, double west){
+
+	}
+
+	public Map(double north, double south, double east, double west, Point points [])
+	{
+		
+	}
+}
