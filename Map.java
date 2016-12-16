a32b0eccf6eb78a960dc1e9ab31142541b58b0b8
Paul K Turner <turner.paul.k@gmail.com>
2016-12-16 17:04:47 -0500

    This will be a test for BB

--- Map.java
+++ Map.java
@@ -4,6 +4,7 @@ public class Map{
 	double south;
 	double east;
 	double west;	
+	//Point resizable array
 
 	public Map(){
 		north = 0;
@@ -11,12 +12,15 @@ public class Map{
 		east = 0;
 		west = 0;
 
-		
+		//Point resizable array	
 	}
 
 	public Map(Point points[]){
-		
-		
+		//Point resizable array = points
+		north = findNorth(points);
+		south = findSouth(points);
+		east = findEast(points);
+		west = findWest(points);
 
 	}
 
