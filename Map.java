2a1b66ea72a80c65434fc90a2993e27c5d15930a
Paul K Turner <turner.paul.k@gmail.com>
2016-12-16 17:59:42 -0500

    Created basic functionality of the map class.

--- Map.java
+++ Map.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 public class Map{
 	
 	double north;
@@ -25,11 +26,62 @@ public class Map{
 	}
 
 	public Map(double north, double south, double east, double west){
-
+		this.north = north;
+		this.south = south;
+		this.east = east;
+		this.west = west;
+		
+		//Points array
 	}
 
 	public Map(double north, double south, double east, double west, Point points [])
 	{
+		this.north = north;
+		this.south = south;
+		this.east = east;
+		this.west = west;
+		//points array 
+	}
+
+	public Point createPossible(Vessel vessel){
 		
+	}
+
+	private static double findNorth(Point points []){
+		double small;
+		for(int i = 0; i < points.length -1;i++){
+			if(small > point.getLatitude())
+				small = point.getLatitude();
+		}
+		return small;
+	}
+	
+	
+
+	private static double findSouth(Point points []){
+		double large;
+		for(int i = 0; i < points.length; i++){
+			if(large < point.getLatitude())
+				large = point.getLatitude();
+		}
+		return large;
+	}
+
+	private static double findWest(Point points []){
+		double small;
+		for(int i = 0; i < points.length; i++){
+			if(small > point.getLongitude())
+				small = point.getLongitude();
+		}
+		return small;
+	}
+	
+	private static double findEast(Point points []){
+		double large;
+		for(int i = 0; i < points.length; i++){
+			if(large < point.getLongitude())
+				large = point.getLongitude();
+		}
+		return large;
 	}
 }
