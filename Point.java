2bc46f62687ee871178224e8eb855c0f6054bd40
Queen <kamalt4@students.rowan.edu>
2016-12-17 11:45:24 -0500

    Add files via upload

--- Point.java
+++ Point.java
@@ -1,33 +1,37 @@
-/*
- * Purpose: The Point class holds the x, y coordinates of the ship.
- * @author: Brooke Brown
- * @version: 2016.12.16
+package team2.lostatsea;
+
+/**
+ * Created by TAKKA on 12/16/2016.
  */
- 
+
 public class Point {
-	double x;
-	double y;
 
-	public Point(double x, double y) {
-		this.x = x;
-		this.y = y;
-	}
+        double x;
+        double y;
 
-	public double getX() 
-	{
-		return x;
-	}
+        public Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
 
-	public double getY()
-	{
-		return y;
-	}
+        public double getX()
+        {
+            return x;
+        }
 
-	public int getLong(){
-		return x-(int)x > .5 ? (int)x+1 : (int)x; //If the long has a value greater than .5 in the decimal then round up, else round down
-	}
-	
-	public int getLat(){
-		return y-(int)y > .5 ? (int)y+1 : (int)y;//If the long has a value greater than .5 in the decimal then round up, else round down
-	}
+        public double getY()
+        {
+            return y;
+        }
+    public void setX(double x)
+    {
+        this.x=x;
+    }
+
+    public void setY(double y)
+    {
+        this.y=y;
+    }
+
+
 }
--- Vessel.java
+++ Vessel.java
@@ -1,86 +1,89 @@
+package team2.lostatsea;
+
+/**
+ * Created by TAKKA on 12/16/2016.
+ */
+
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Vessel {
-	
-Date tNot;
-Point lastLoc; 
 
-Double speed; 
 
-Double heading; 
+    String Name;
 
-Date ETA; 
+    Date tNot;
+    Point lastLoc;
 
-public Vessel(Date tNot, Point lastLoc, Double speed, Double heading, Date ETA ) {
-	DateFormat inputFormat = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss SS");
-	this.tNot= tNot; 
-	this.lastLoc =lastLoc; 
-	this.speed = speed; 
-	this.heading=heading; 
-	this.ETA=ETA; 
-	
-	
+    double speed;
+
+    double heading;
+
+    Date ETA;
+
+    public Vessel(Date tNot, Point lastLoc, double speed, double heading, Date ETA, String Name ) {
+        DateFormat inputFormat = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss SS");
+        this.tNot= tNot;
+        this.lastLoc =lastLoc;
+        this.speed = speed;
+        this.heading=heading;
+        this.ETA=ETA;
+        this.Name = Name;
+
+
+
+    }
+public String Name(){
+    return Name;
 }
+    public Date gettNot() {
+        return tNot;
+    }
 
-public Date gettNot() {
-	return tNot;
+    public void settNot(Date tNot) {
+        this.tNot = tNot;
+    }
+
+    public Point getLastLoc() {
+        return lastLoc;
+    }
+
+    public void setLastLoc(Point lastLoc) {
+        this.lastLoc = lastLoc;
+    }
+
+    public Double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(Double speed) {
+        this.speed = speed;
+    }
+
+    public Double getHeading() {
+        return heading;
+    }
+
+    public void setHeading(Double heading) {
+        this.heading = heading;
+    }
+
+    public Date getETA() {
+        return ETA;
+    }
+
+    public void setETA(Date eTA) {
+        ETA = eTA;
+    }
+
+    public double getLongitude() {
+        return lastLoc.getX();
+    }
+
+    public double getLatitude() {
+        return lastLoc.getY();
+    }
+
 }
-
-public void settNot(Date tNot) {
-	this.tNot = tNot;
-}
-
-public Point getLastLoc() {
-	return lastLoc;
-}
-
-public void setLastLoc(Point lastLoc) {
-	this.lastLoc = lastLoc;
-}
-
-public Double getSpeed() {
-	return speed;
-}
-
-public void setSpeed(Double speed) {
-	this.speed = speed;
-}
-
-public Double getHeading() {
-	return heading;
-}
-
-public void setHeading(Double heading) {
-	this.heading = heading;
-}
-
-public Date getETA() {
-	return ETA;
-}
-
-public void setETA(Date eTA) {
-	ETA = eTA;
-}
-
-public double getX() {
-	return lastLoc.getX();
-}
-
-public double getY() {
-	return lastLoc.getY();
-}
-
-	public int getLong()
-	{
-		return (int) lastLoc.getY();
-	}
-
-	public int getLat()
-	{
-		return (int) lastLoc.getX();
-	}
-	
-}
-
